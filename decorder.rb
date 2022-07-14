722b7401577fbaf0a44ccc0651e16022f6a1b510
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-14 18:45:45 +0200

    Add decoder logic

--- decorder.rb
+++ decorder.rb
@@ -37,3 +37,34 @@ MORSE_DICTIONARY = {
     '----.' => '9',
     '-----' => '0'
   }.freeze
+
+
+  #   The following fucntion convert a morse letter into alphabetic
+  def convert_char(char)
+    return  MORSE_DICTIONARY[char].capitalize
+    
+    end
+  #   The following fucntion convert a morse word into an alphabetic word
+    def convert_word(word)
+      if word.is_a? String
+        word_converted = ''
+        word.split.each do |morse|
+          dc = convert_char(morse)
+          word_converted += dc.to_s
+        end
+      end
+      word_converted
+    end
+  #   The following fucntion convert a morse sentences into an alphabetic word
+    def convert_sentence(sentence)
+      if sentence.is_a? String
+          sentence_converted=''
+          sentence.split('   ').each do |text|
+             dc= convert_word(text)
+             sentence_converted +="#{dc} "
+          end
+      end
+      sentence_converted
+  end
+  # Testing
+    puts convert_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
